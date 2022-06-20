3c9502a35b2d2b8dd2640a8f3de432eabd1f11b4
Nasko <103703875+ArmaNasko@users.noreply.github.com>
2022-06-20 03:34:38 +0200

    Add files via upload

---
+++ shared/shared.lua
@@ -0,0 +1,13 @@
+Laboratory = {
+    {
+        name = "Weed",
+        label1 = "weed_seed",
+        label2 = "weed_poch",
+        color = "~g~",
+        enterPos = {x = 2589.4, y = 4678.2, z = 34.1},
+        exitPos = {x = 1066.2, y = -3183.5, z = -39.2},
+        recoltePos = {x = 1057.4, y = -3197.2, z = -39.1},
+        traitementPos = {x = 1039.2, y = -3205.3, z = -38.2},
+        markersColor = {r = 65, g = 210, b = 40},
+    },
+}
